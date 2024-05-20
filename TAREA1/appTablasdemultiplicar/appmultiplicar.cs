069690cf14e46ad5d7db6f08a21bf2874dec090f
Charly1987fc <chajo87@gmail.com>
2024-05-20 15:15:47 -0600

    Add files via upload

---
+++ TAREA1/appTablasdemultiplicar/appmultiplicar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appTablasdemultiplicar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero,resultado;
+            Console.WriteLine("Ingrese un valor numerico");
+  numero=int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++) 
+            {
+                resultado=numero*i;
+            Console.WriteLine(numero+"x"+i+"="+resultado );
+            }
+            Console.ReadLine();
+        }
+    }
+}
